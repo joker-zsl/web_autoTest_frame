0636e5e48dd761247754168a97c4bc879e99d5bf
joker-zsl <clown6@sina.cn>
2021-05-10 22:41:17 +0800

    优化beautifulReport

--- frame/core/BeautifulReport/BeautifulReport.py
+++ frame/core/BeautifulReport/BeautifulReport.py
@@ -6,6 +6,8 @@ import json
 import unittest
 import traceback
 from io import StringIO as StringIO
+
+from frame.utils.handle_log import log
 from frame.utils.handle_path import TIME_REPORT_PATH
 
 __all__ = ['BeautifulReport']
@@ -15,13 +17,13 @@ class OutputRedirector(object):
     """ Wrapper to redirect stdout or stderr """
     def __init__(self, fp):
         self.fp = fp
-    
+
     def write(self, s):
         self.fp.write(s)
-    
+
     def writelines(self, lines):
         self.fp.writelines(lines)
-    
+
     def flush(self):
         self.fp.flush()
 
@@ -41,7 +43,7 @@ resultData = {
     "testSkip": 0
 }
 
-beautiful_report_dir_path = os.path.abspath(os.path.dirname(os.path.dirname(__file__)))
+beautiful_report_dir_path = os.path.abspath(os.path.dirname(__file__))
 template_path = os.path.join(beautiful_report_dir_path, 'template.html')  # 模板文件路径
 
 
@@ -53,10 +55,10 @@ class MakeResultJson:
         """
         self.datas = datas
         self.result_schema = {}
-    
+
     def __setitem__(self, key, value):
         self[key] = value
-    
+
     def __repr__(self) -> str:
         """
         :return: 返回一个构造完成的表单th数据
@@ -76,9 +78,7 @@ class ReportTestResult(unittest.TestResult):
         self.stream = stream
         self.end_time = 0
         self.failure_count = 0
-        self.error_count = 0
         self.success_count = 0
-        self.skipped = []
         self.suite = suite
         self.status = ''
         self.default_report_name = '自动化测试报告'
@@ -89,12 +89,13 @@ class ReportTestResult(unittest.TestResult):
         self.result_list = []
         self.case_log = []
         self._mirrorOutput = False  # print(class_name, method_name, method_doc)
-    
+        self.logger = log
+
     @property
     def success_counter(self) -> int:
         """设置成功次数"""
         return self.success_count
-    
+
     @success_counter.setter
     def success_counter(self, value) -> None:
         """
@@ -103,7 +104,7 @@ class ReportTestResult(unittest.TestResult):
         :return:
         """
         self.success_count = value
-    
+
     def startTest(self, test) -> None:
         """
         测试用例运行前调用
@@ -113,11 +114,14 @@ class ReportTestResult(unittest.TestResult):
         stdout_redirector.fp = self.outputBuffer
         stderr_redirector.fp = self.outputBuffer
         self.sys_stdout = sys.stdout
-        self.sys_stdout = sys.stderr
+        self.sys_stderr = sys.stderr
         sys.stdout = stdout_redirector
         sys.stderr = stderr_redirector
+        for handler in self.logger.handlers:
+            if handler.stream == self.sys_stderr:
+                handler.stream = sys.stderr
         self.start_time = time.time()
-    
+
     def stopTest(self, test) -> None:
         """
         测试用例执行后调用
@@ -125,38 +129,40 @@ class ReportTestResult(unittest.TestResult):
         self.end_time = '{0:.3} s'.format((time.time() - self.start_time))
         self.result_list.append(self.get_all_result_info_tuple(test))
         self.complete_output()
-    
+
     def complete_output(self):
         """
         断开输出重定向
         """
         if self.sys_stdout:
+            for handler in self.logger.handlers:
+                if handler.stream == sys.stderr:
+                    handler.stream = self.sys_stderr
             sys.stdout = self.sys_stdout
-            sys.stderr = self.sys_stdout
+            sys.stderr = self.sys_stderr
             self.sys_stdout = None
-            self.sys_stdout = None
+            self.sys_stderr = None
         return self.outputBuffer.getvalue()
-    
+
     def stopTestRun(self, title=None) -> dict:
         """
-            所有测试执行完成后, 执行该方法
+        所有测试执行完成后, 执行该方法
         :param title:
         :return:
         """
-        resultData['testPass'] = self.success_counter
         for item in self.result_list:
             item = json.loads(str(MakeResultJson(item)))
             resultData.get('testResult').append(item)
-        resultData['testAll'] = len(self.result_list)
         resultData['testName'] = title if title else self.default_report_name
+        resultData['testAll'] = len(self.result_list)
+        resultData['testPass'] = self.success_counter
         resultData['testFail'] = self.failure_count
         resultData['beginTime'] = self.begin_time
         end_time = int(time.time())
         start_time = int(time.mktime(time.strptime(self.begin_time, '%Y-%m-%d %H:%M:%S')))
-        resultData['totalTime'] = str(end_time - start_time) + 's'
-        resultData['testError'] = self.error_count
-        resultData['testSkip'] = self.skipped
-        self.resultData = resultData
+        m, s = divmod(int(end_time - start_time), 60)
+        h, m = divmod(m, 60)
+        resultData['totalTime'] = "{:02}时 {:02}分 {:02}秒".format(h, m, s)
         return resultData
 
     @staticmethod
@@ -182,14 +188,14 @@ class ReportTestResult(unittest.TestResult):
         except AttributeError:
             case_images = []
         return tuple([*self.get_testcase_property(test), self.end_time, self.status, self.case_log, case_images])
-    
+
     @staticmethod
     def error_or_failure_text(err) -> str:
         """
         获取sys.exc_info()的参数并返回字符串类型的数据, 去掉t6 error
         """
         return traceback.format_exception(*err)
-    
+
     def addSuccess(self, test) -> None:
         logs = []
         output = self.complete_output()
@@ -199,35 +205,27 @@ class ReportTestResult(unittest.TestResult):
         self.status = '成功'
         self.case_log = output.split('\n')
         self._mirrorOutput = True
-    
+
     def addError(self, test, err):
         logs = []
         output = self.complete_output()
-        logs.append(output)
+        logs.extend(output.split('\n'))
         logs.extend(self.error_or_failure_text(err))
         self.failure_count += 1
         self.add_test_type('失败', logs)
-        sys.stderr.write('F')
+        sys.stderr.write('E')
         self._mirrorOutput = True
-    
+
     def addFailure(self, test, err):
         logs = []
         output = self.complete_output()
-        logs.append(output)
+        logs.extend(output.split('\n'))
         logs.extend(self.error_or_failure_text(err))
         self.failure_count += 1
         self.add_test_type('失败', logs)
         sys.stderr.write('F')
         self._mirrorOutput = True
-    
-    def addSkip(self, test, reason) -> None:
-        logs = [reason]
-        self.complete_output()
-        self.skipped += 1
-        self.add_test_type('跳过', logs)
-        sys.stderr.write('S')
-        self._mirrorOutput = True
-    
+
     def add_test_type(self, status: str, case_log: list) -> None:
         self.status = status
         self.case_log = case_log
@@ -245,7 +243,7 @@ class BeautifulReport(ReportTestResult):
         self.suites.run(result=self)
         self.stopTestRun(description)
         self.output_report()
-    
+
     def output_report(self):
         """
         输出测试报告
@@ -254,8 +252,11 @@ class BeautifulReport(ReportTestResult):
         filename = os.path.join(TIME_REPORT_PATH, f"report_{day}.html")
         with open(template_path, 'r', encoding='utf-8') as file:
             body = file.read()
-        with open(filename, 'w') as write_file:
+        with open(filename, 'w', encoding='utf-8') as write_file:
             item = re.search('\${resultData}', body).group()
             template = body.replace(item, json.dumps(self.resultData, ensure_ascii=False, indent=4))
             write_file.write(template)
 
+
+if __name__ == '__main__':
+    print(beautiful_report_dir_path)
